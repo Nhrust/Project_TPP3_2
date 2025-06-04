74e00713b7e9050bcb0c8e59404dd2c415a3cfe3
Nhrust <nhrustalev2024@gmail.com>
2025-06-04 13:11:33 +0300

    HOT FIX !!!

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from source.pages    import *
 from source.requests import*
 
 if not accounts.check_login("admin"):
-	admin = Account(base, "admin", 6463131979109318604)
+	admin = Account(base, "admin", 6463131979109318604, "")
 	admin.name = "Админ"
 	admin.update_on_base(base)
 
